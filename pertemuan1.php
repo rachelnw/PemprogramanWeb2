f178d48aed70bdf9e73e14eef6ad1efbdaa48837
rachelnw <rachelnabw@gmail.com>
2024-03-03 17:50:17 +0700

    commit pertama

---
+++ pertemuan1.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h1 style='color: red; size; 14; text-align: left;'>BIODATA</h1>";
+echo "<span style='color: blue;'>Nama : </span><span style='color: blue; text-align: center;'><strong>Rachel Nabila Wijaya</strong></span><br>";
+echo "<span style='color: blue;'>Email :</span> <span style='color: blue;'>rachelnabw@gmail.com</span><br>";
+echo "<span style='color: blue;'>Jurusan :</span> <span style='color: blue;'>Teknik Informatika</span><br>";
+echo "<span style='color: blue;'>Hoby :</span> <span style='color: blue;'>Travelling</span><br>";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pertemuan 1</title>
+</head>
+<body>
+<?php
+$message = "Cukup sulit namun menyenangkan, tapi harus banyak latihan agar bisa";
+echo "<h1 style='text-align: left; color: red; size: 10;'>Kesan Saya Belajar PHP</h1>";
+echo "<p>$message</p>";
+?>
+</body>
+</html>
